433236a642c291254fef12e81b0e17540805ca4e
user <user@Sam-2.local>
2026-02-16 00:02:29 +0500

    resource group fix

--- Multi-cloud_Infrastucture/route_tables.tf
+++ Multi-cloud_Infrastucture/route_tables.tf
@@ -1,7 +1,7 @@
 resource "azurerm_route_table" "indiac-indiag" {
   name                = "indiac-indiag"
-  location            = azurerm_resource_group.windows-ad.location
-  resource_group_name = azurerm_resource_group.windows-ad.name
+  location            = azurerm_resource_group.windows-client.location
+  resource_group_name = azurerm_resource_group.windows-client.name
 
   route {
     name           = "windowsclient-gateway"
@@ -16,14 +16,14 @@ resource "azurerm_route_table" "indiac-indiag" {
 }
 
 resource "azurerm_subnet_route_table_association" "ad_assoc" {
-  subnet_id      = azurerm_subnet.windows-ad.id
+  subnet_id      = azurerm_subnet.windows-client.id
   route_table_id = azurerm_route_table.indiac-indiag.id
 }
 
 resource "azurerm_route_table" "norway-india" {
   name                = "norway-india"
-  location            = azurerm_resource_group.windows-client.location
-  resource_group_name = azurerm_resource_group.windows-client.name
+  location            = azurerm_resource_group.windows-ad.location
+  resource_group_name = azurerm_resource_group.windows-ad.name
 
   route {
     name           = "ad-gateway"
@@ -38,6 +38,6 @@ resource "azurerm_route_table" "norway-india" {
 }
 
 resource "azurerm_subnet_route_table_association" "client-assoc" {
-  subnet_id      = azurerm_subnet.windows-client.id
+  subnet_id      = azurerm_subnet.windows-ad.id
   route_table_id = azurerm_route_table.norway-india.id
 }
